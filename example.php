54da08c803c80a48f469da469fd4fac4eabd920a
mcm_ham_cp <SND\mcm_ham_cp@localhost>
2009-12-07 01:46:01 -0800

    Add php example.

--- example.php
+++ example.php
@@ -1,7 +1,18 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="example.aspx.cs" Inherits="example" %><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php
+	if ($_POST) {
+		$vals = explode("|", $_POST["ids"]);
+		for ($idx = 0; $idx < count($vals); $idx+=1) {
+			$id = $vals[$idx];
+			$ordinal = $idx;
+			//...
+		}
+		return;
+	}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
+<head>
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
 	<style type="text/css">
 		body { font-family:Arial; font-size:12pt; padding:20px; width: 800px; margin:20px auto; border:solid 1px black; }
@@ -10,14 +21,13 @@
 		ul { width:350px; list-style-type: none; margin:0px; padding:0px; }
 		li { float:left; padding:5px; width:100px; height:100px; }
 		li div { width:90px; height:50px; border:solid 1px black; background-color:#E0E0E0; text-align:center; padding-top:40px; }
-		.placeHolder div { background-color:white !important; border:dashed 1px gray !important; }
+		.placeHolder div { background-color:white!important; border:dashed 1px gray !important; }
 	</style>
 </head>
 <body>
-    <form id="form1" runat="server">
     <div>
         
-        <h1>jQuery List DragSort ASP.NET Example</h1>
+        <h1>jQuery List DragSort PHP Example</h1>
 	    
 	    <a href="http://dragsort.codeplex.com/">Homepage</a><br/>
 	    <br/>
@@ -25,13 +35,14 @@
         <h2>Save list order with ajax:</h2>
         
         <ul id="gallery">
-            <asp:Repeater ID="Gallery" runat="server">
-                <ItemTemplate>
-                    <li itemID='<%# Container.ItemIndex %>'>
-                        <div><%# Container.DataItem %></div>
-                    </li>
-                </ItemTemplate>
-            </asp:Repeater>
+			<?php
+				$list = array("blue", "orange", "brown", "red", "yellow", "green", "black", "white", "purple");
+				for ($idx = 0; $idx < count($list); $idx+=1) {
+					echo "<li itemID='" . $idx . "'>";
+					echo "<div>" . $list[$idx] . "</div>";
+					echo "</li>";
+				}
+			?>
 		</ul>
 		
 		<script type="text/javascript" src="jquery.dragsort.js"></script>
@@ -41,12 +52,11 @@
 		    function saveOrder() {
 		        var serialStr = "";
 		        $("#gallery li").each(function(i, elm) { serialStr += (i > 0 ? "|" : "") + $(elm).attr("itemID"); });
-		        $.ajax({ url: "example.aspx/SaveListOrder", data: '{"ids":"' + serialStr + '"}', dataType: "json", type: "POST", contentType: "application/json; charset=utf-8" });
+		        $.post("example.php", { ids: serialStr });
 		    };
 	    </script>
         
         <div style="clear:both;"></div>
     </div>
-    </form>
 </body>
 </html>
